1361c18d94aa518e4dbe24fd6999d4e894c2ef8a
Vladimir Kozhevnikov <44900317+UnrealDrunk@users.noreply.github.com>
2019-01-14 22:30:54 +0300

    Create 1_to_10.cpp

---
+++ 1_to_10.cpp
@@ -0,0 +1,17 @@
+//Write a program that displays natural numbers from 1 to 10 to the console
+
+#include<iostream>
+
+int main()
+{
+	const int ARR_LENGHT = 10;
+	
+	for (int i = 1; i <= ARR_LENGHT; i++)
+	{
+		std::cout << i << " ";
+
+	}
+
+	getchar();
+
+}
