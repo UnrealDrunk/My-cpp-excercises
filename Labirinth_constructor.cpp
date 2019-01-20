ed6ac27f8433e47097b0240d419bad073a401560
Vladimir Kozhevnikov <44900317+UnrealDrunk@users.noreply.github.com>
2019-01-20 13:38:32 +0300

    Create Labirinth_constructor.cpp
    This program constructs labirinth, using ASCII symbols

---
+++ Labirinth_constructor.cpp
@@ -0,0 +1,64 @@
+// This program constructs a labirinth
+
+#include<iostream>
+
+const int Arr_field = 10;
+
+
+int main()
+{
+	// 1 - стенки лабиринта
+	// 0 - путь
+	int Lab_Construct[Arr_field][Arr_field] =
+	{ 
+		{1,1,1,1,1,1,1,1,1,1,},
+		{1,1,0,0,0,0,0,0,0,1,},
+		{0,0,0,1,1,1,1,0,1,1,},
+		{1,1,1,1,0,0,1,0,0,1,},
+		{1,0,0,1,0,1,1,1,0,1,},
+		{1,0,1,1,0,0,0,0,0,1,},
+		{1,0,1,1,0,1,1,1,1,1,},
+		{1,0,0,0,0,0,0,0,1,1,},
+		{1,0,1,1,0,1,1,0,0,0,},
+		{1,1,1,1,1,1,1,1,1,1,} 
+	};
+	int Door_s1 = 8;
+	int Door_s2 = 9;
+	Lab_Construct[Door_s1][Door_s2] = 2;
+
+	do
+	{
+		for (int i = 0; i < Arr_field; i++) //переключение по строкам
+		{
+			for (int j = 0; j < Arr_field; j++)// переключение по столбцам
+			{
+				if (Lab_Construct[i][j] == 1)
+				{
+					
+					std::cout << '\xDB'; // static_cast<char>(219);
+					std::cout << '\xDB'; // static_cast<char>(219);
+				}
+				if (Lab_Construct[i][j] == 0)
+				{
+					std::cout << "  ";
+				}
+				if (Lab_Construct[i][j] == 2)
+				{
+					std::cout << '\xB6'; // static_cast<char>(182);
+					std::cout << '\xB6'; //static_cast<char>(182);
+				}
+			}
+			std::cout << '\n';
+		}
+
+		std::cin.get();
+	} while (true);
+
+	std::getchar();
+
+
+
+
+
+
+}
